57e5246fc722e019c9a74cb1d02f6e0b6f3f1dd4
HajiSalman <faridz4life@gmail.com>
2021-06-11 14:14:21 +0700

    a

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,14 +20,14 @@
   <!-- Main content -->
   <section class="content">
     <div class="container-fluid">
-      <!-- Main row -->
+        
+        <!-- Main row -->
       <div class="row">
         <div class="col-lg-3 col-6">
           <!-- small box -->
           <div class="small-box bg-info">
             <div class="inner">
               <h3>150</h3>
-
               <p>New Orders</p>
             </div>
             <div class="icon">
